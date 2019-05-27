5bdbd10f4ca48d0ada8ff0f2cfeee5d062404b85
liujianran <liu.jianran@163.com>
2019-05-27 22:21:55 +0800

    up

---
+++ make.sh
@@ -0,0 +1,12 @@
+SH_FILE_PATH=$(pwd)
+echo $SH_FILE_PATH
+echo "=========================================="
+rm -rf $SH_FILE_PATH/build
+mkdir $SH_FILE_PATH/build
+cd $SH_FILE_PATH/build
+# cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8
+echo "make mpl-cpp FINISHED"
+echo "=========================================="
+cd $SH_FILE_PATH
